a30da4af38737b16bb8f4e75e1fd9e9392206abd
Mariaxvr <mariavkrohnq@gmail.com>
2025-11-16 19:57:18 +0100

    hausaufgabe 5

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -3,5 +3,23 @@ package h1;
 public class H1_Main {
     public static void main(String[] args) {
 
+        int [] myArray = {1,2,3,4,5};
+
+        System.out.println("Vorher: ");
+        for (int value : myArray) {
+            System.out.println(value); //wert vor dem umderehen
+        }
+        System.out.println(); //leere zeile
+
+        for (int i = 0; i< myArray.length/2; i++){
+            int temp = myArray[i];
+            myArray[i] = myArray[myArray.length-1-i];
+            myArray[myArray.length-1-i] = temp;
+        }
+        System.out.println("Nachher: ");
+        for (int value : myArray) {
+            System.out.println(value);
+        }
+
     }
 }
