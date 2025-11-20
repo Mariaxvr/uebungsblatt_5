f744eff15f1a10f1c6752972cbcebc592baa95f4
Mariaxvr <mariavkrohnq@gmail.com>
2025-11-20 14:48:58 +0100

    hausaufgabe 5

--- src/h2/H2_Main.java
+++ src/h2/H2_Main.java
@@ -3,9 +3,32 @@ package h2;
 public class H2_Main {
     public static void main(String[] args) {
 
-        int n;
-        int digits;
-        int[] a = new int [9];
+        int n = 299;
+        int digits = 0;
+        int[] a = new int [9]; //automatisch mit 0 vorbelegt
+
+        if (n == 0) { //anzahl der ziffern bestimmen
+            digits = 1;
+        } else {
+            int temp = n;
+            while (temp > 0) {
+                digits++;
+                temp /= 10;
+            }
+        }
+        int index = 8;     //füllen von rechts nach links
+        int temp = n;
+
+        while (temp > 0) { //ziffern in das array ablegen
+            a[index] = temp % 10;  // letzte Ziffer
+            temp /= 10;            // entferne letzte Ziffer
+            index--;
+        }
+        System.out.println("digits = " + digits);
+        System.out.print("Array a = ");
+        for (int value : a) {
+            System.out.print(value + " ");
+        }
 
 
     }
